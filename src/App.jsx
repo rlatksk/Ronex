fd566c275fec6f9e036b6ecc1f568c379ec8b0ff
rlatksk <justinsalim73@gmail.com>
2025-08-06 20:34:44 +0700

    Change HashRouter into BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Navbar from './components/Navbar';
